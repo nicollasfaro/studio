ceaabc0200c22b74820e100faa83861249779d1a
Faro Ciuldin <nicollasciuldin@gmail.com>
2025-10-03 20:06:59 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1120078662-2af08",
+  "appId": "1:782477450219:web:35c4aea7a95eeba5e557ae",
+  "apiKey": "AIzaSyAy725ooH734-1cjxR30jCDd9XaJrmcD48",
+  "authDomain": "studio-1120078662-2af08.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "782477450219"
+};
