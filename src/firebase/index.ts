c90f99c6cdfb16b72cb1882e20fd9151c6c40417
Faro Ciuldin <nicollasciuldin@gmail.com>
2025-10-06 01:25:57 +0000

    Try fixing this error: `Build Error: Export useUserData doesn't exist in

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -46,5 +46,6 @@ export * from './provider';
 export * from './client-provider';
 export * from './firestore/use-collection';
 export * from './firestore/use-doc';
+export * from '@/hooks/use-user-data';
 export * from './errors';
 export * from './error-emitter';
